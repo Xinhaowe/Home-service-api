eb1dd0fc10773213f3e4f23ae2ba8c73d38756c9
Harvey <xinhaowei68@gmail.com>
2024-11-05 12:36:11 +1030

    commit to add npm core

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+app.use(cors());
 
 // Serve the JSON data at the `/services` endpoint
 app.get("/services", (req, res) => {
